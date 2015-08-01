475a62660dc6d060b28211e5a1ba093ffc8e24ca
dominaki <dominaki@gmail.com>
2015-08-01 11:44:10 -0500

    Full Functionality
    Working app.  Would like to add some style.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,44 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+ def create_row
+
+    p = Photo.new
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+
+    
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+def show
+	@photo = Photo.find(params["id"])
+	render("show.html.erb")
+end
+
+ def edit_form
+    @photo = Photo.find(params["id"])
+  end
+
+def update_photo
+	p = Photo.find(params["id"])
+	p.caption = params["the_caption"]
+    p.source = params["the_source"]
+	p.save
+
+	redirect_to("http://localhost:3000/photos")
 
 end
+
+def destroy
+    @photo = Photo.find(params["id"])
+
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,18 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+
   get("/photos",           { :controller => "photos", :action => "index" })
+  
+  get("/photos/create_photo/",      { :controller => "photos", :action => "create_row" })
+  get("/photos/new",        { :controller => "photos", :action => "new_form"})
+
+
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_photo"})
+  
 
 end
